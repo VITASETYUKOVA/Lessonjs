bcc9f6bfe1cd9ba65e7729cc5215c07d8ac68f04
Setiukova VIKTORIIA <_viktoriya-@ukr.net>
2024-09-05 16:12:41 +0300

    Task 2

---
+++ main.js
@@ -0,0 +1,4 @@
+let firstString = prompt( "First string");
+let secondString = prompt( "Second string");
+let thirdString = prompt( "Third string");
+console.log(`${firstString}, ${secondString}, ${thirdString}`);
