7bb304a3938f8cf75481ed68c84479a2d821f612
Setiukova VIKTORIIA <_viktoriya-@ukr.net>
2024-09-13 08:41:19 +0300

    Lesson 5 task 3

---
+++ main.js
@@ -0,0 +1,12 @@
+let N = parseInt(prompt(""));
+let result = "";
+
+for (let i = 1; i <= 100; i++) {
+    if (i * i  <= N){
+        result += i + " ";
+    } else {
+        break;
+    }
+}
+    console.log (result);
+
