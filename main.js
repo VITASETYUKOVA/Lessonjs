013a61457202acc36660476534dc9defcac6cb80
Setiukova VIKTORIIA <_viktoriya-@ukr.net>
2024-09-26 22:50:01 +0300

    Lesson 8 task 1

---
+++ main.js
@@ -0,0 +1,27 @@
+let ladder = {
+    current: 0,
+  
+    up: function () {
+      this.current++; 
+      return this;    
+    },
+  
+    down: function () {
+      this.current--; 
+      return this;    
+    },
+  
+    showStep: function () {
+      console.log(this.current); 
+      return this;               
+    }
+  };
+  
+  const actions = ['up', 'up', 'down'];
+  
+  actions.filter(action => typeof ladder[action] === 'function') 
+  .forEach(action => ladder[action]()); 
+  
+  ladder.current = 0; 
+  
+  ladder.up().up().down().showStep(); 
