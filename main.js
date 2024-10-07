ab5fd61b7a15fe5fc5ea1a362124cb2937afcc40
Setiukova VIKTORIIA <_viktoriya-@ukr.net>
2024-10-07 23:45:51 +0300

    Lesson 11 task 1

---
+++ main.js
@@ -0,0 +1,14 @@
+const newTable = document.querySelector('#myTable'); 
+
+for (let i = 0; i < 10; i++) {
+  const newTr = document.createElement('tr'); 
+
+  for (let j = 0; j < 10; j++) {
+    const newTd = document.createElement('td'); 
+    newTr.appendChild(newTd); 
+  }
+
+  newTable.appendChild(newTr); 
+}
+
+console.log(newTable);
