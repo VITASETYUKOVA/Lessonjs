0287695fc4338bfb1f110e74a912d1731e323dfe
Setiukova VIKTORIIA <_viktoriya-@ukr.net>
2024-12-08 20:08:56 +0200

    Lesson 28.1 fixed

--- todo/src/components/Layout/Layout.js
+++ todo/src/components/Layout/Layout.js
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { NavLink, Outlet } from "react-router-dom";
 import { ThemeContext } from "../../context/ThemeContext";
-import './Layout.css';
+import "./Layout.css";
 
 export default function Layout() {
   const { theme, toggleTheme } = useContext(ThemeContext);
@@ -9,12 +9,17 @@ export default function Layout() {
   return (
     <div className={`container ${theme}`}>
       <header className={`header-${theme}`}>
-        <NavLink to="/" className="nav-link">Головна</NavLink>
-        <NavLink to="/contacts" className="nav-link">Контакти</NavLink>
-        <NavLink to="/about" className="nav-link">Про мене</NavLink>
+        <NavLink to="/" className="nav-link">
+          Головна
+        </NavLink>
+        <NavLink to="/contacts" className="nav-link">
+          Контакти
+        </NavLink>
+        <NavLink to="/about" className="nav-link">
+          Про мене
+        </NavLink>
         <button onClick={toggleTheme}>Змінити тему</button>
       </header>
-
       <div className="main-content">
         <Outlet />
       </div>
