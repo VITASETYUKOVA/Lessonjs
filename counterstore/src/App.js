a70db573bae241b22d56badacd171c72aa31edbe
Setiukova VIKTORIIA <_viktoriya-@ukr.net>
2024-12-15 18:09:03 +0200

    Lesson 29 task 1

---
+++ counterstore/src/App.js
@@ -0,0 +1,8 @@
+import Counter from "./components/Counter";
+import "./index.css";
+
+const App = () => {
+  return <Counter />;
+};
+
+export default App;
---
+++ counterstore/src/components/Counter.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "../slices/CounterSlice";
+
+const Counter = () => {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="container">
+      <h1>Value: {count}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+    </div>
+  );
+};
+
+export default Counter;
