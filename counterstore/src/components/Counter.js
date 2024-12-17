02abec0302cfb23ca5c90ca2012a2757548b6f7e
Setiukova VIKTORIIA <_viktoriya-@ukr.net>
2024-12-17 14:47:50 +0200

    Fixed lesson 29

--- counterstore/src/components/Counter.js
+++ counterstore/src/components/Counter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "../slices/CounterSlice";
+import { increment, decrement } from "../store/slices/counterSlice";
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.count);
